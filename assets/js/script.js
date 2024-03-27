cc873346343dfc7cfeb96b74150446419a1700ee
dolmerfresh <146840972+dolmerfresh@users.noreply.github.com>
2024-03-27 09:53:32 +0100

    remove: Removed start button

--- assets/js/script.js
+++ assets/js/script.js
@@ -36,19 +36,7 @@ const cars = [
 const categories = [animals, foods, culturalObjects, cars];
 
 // Wait for the DOM to finish loading before running the game
-// Get the button elements and add event listeners to them
 document.addEventListener("DOMContentLoaded", function() {
-    let buttons = document.getElementsByTagName("button");
- 
-    for (let button of buttons) {
-        button.addEventListener("click", function() {
-            if (this.getAttribute("data-type") === "submit") {
-                checkAnswer();
-            } else if (this.getAttribute("data-type") === "start") {
-                runGame();
-            }
-        })
-    }
     runGame();
 })
 
